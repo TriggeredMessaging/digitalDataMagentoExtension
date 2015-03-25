296d450bae80bdc28306a53a94ace9aa95a0a987
Blake Finney <blake.finney@triggeredmessaging.com>
2015-03-25 09:34:47 +0000

    Final reformat and attribute collection correct

--- app/code/community/TriggeredMessaging/DigitalDataLayer/Model/System/Config/Source/Productattributes.php
+++ app/code/community/TriggeredMessaging/DigitalDataLayer/Model/System/Config/Source/Productattributes.php
@@ -1,6 +1,8 @@
 <?php
+
 class TriggeredMessaging_DigitalDataLayer_Model_System_Config_Source_Productattributes
-{    /**
+{
+    /**
      * Options getter
      *
      * @return array
@@ -8,20 +10,21 @@ class TriggeredMessaging_DigitalDataLayer_Model_System_Config_Source_Productattr
     public function toOptionArray()
     {
         $attributes = Mage::getModel('catalog/product')->getAttributes();
-            $attributeArray = array(array('label'=>'none','value'=>'0'));
+        $attributeArray = array(array('label' => 'none', 'value' => '0'));
 
-            foreach($attributes as $a){
+        foreach ($attributes as $a) {
 
-                foreach ($a->getEntityType()->getAttributeCodes() as $attrCode) {
-                    $attribute_details = Mage::getSingleton("eav/config")->getAttribute('catalog_product', $attrCode);
-                    if($attribute_details->getData('is_user_defined')){
-                        array_push($attributeArray,array('label' => $attrCode,'value' => $attrCode));
-                    }
+            foreach ($a->getEntityType()->getAttributeCodes() as $attrCode) {
+                $attribute_details = Mage::getSingleton("eav/config")->getAttribute('catalog_product', $attrCode);
+                if ($attribute_details->getData('is_user_defined')) {
+                    array_push($attributeArray, array('label' => $attrCode, 'value' => $attrCode));
                 }
-                break;
             }
-            array_push($attributeArray,array('label' => 'weight','value' => weight));
-            return $attributeArray;
+            break;
+        }
+        array_push($attributeArray, array('label' => 'weight', 'value' => 'weight'));
+        return $attributeArray;
     }
 }
+
 ?>
